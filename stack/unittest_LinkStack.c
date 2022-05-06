332758ec619bea7b229d851aeca701694c9acacc
Zncl2222 <3002shinning@gmail.com>
2022-05-07 00:45:02 +0800

    Add new test case

--- stack/unittest_LinkStack.c
+++ stack/unittest_LinkStack.c
@@ -7,10 +7,21 @@ int main(){
 
     LinkStack S;
 
-    CreateStack_Random(&S,10);
+    CreateStack_Random(&S,5);
 
     StackTarvarse(&S);
 
+    for(int i=0; i<5; i++){
+        
+        StackPush(&S,i*i);
+    }
+    
+    StackTarvarse(&S);
+
+    StackPop(&S);
+
+    StackTarvarse(&S);
+    
     system("Pause");
 
     return 0;
