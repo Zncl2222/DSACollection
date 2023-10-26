d8eed00246e83c13578c5fea557d2d602501559b
Zncl2222 <3002shinning@gmail.com>
2023-10-26 20:46:41 +0800

    feat(sort): add bubble sort

---
+++ sorting/bubble_sort/bubble_sort.py
@@ -0,0 +1,5 @@
+def bubble_sort(arr: list):
+    for _ in range(len(arr)):
+        for j in range(len(arr) - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
