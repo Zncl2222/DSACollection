aaca1102190810ca202a0f6987e6fc1bd2dcee93
Zncl2222 <3002shinning@gmail.com>
2023-11-02 21:54:45 +0800

    test(sort): add test for selection_sort

--- py_test/sort_test.py
+++ py_test/sort_test.py
@@ -1,5 +1,6 @@
 import pytest
 from sorting.bubble_sort.bubble_sort import bubble_sort
+from sorting.selection_sort.selection_sort import selection_sort
 
 
 @pytest.mark.parametrize(
@@ -15,4 +16,20 @@ from sorting.bubble_sort.bubble_sort import bubble_sort
 )
 def test_bubble_sort(input_list, expected):
     bubble_sort(input_list)
+    assert input_list == expected
+
+
+@pytest.mark.parametrize(
+    "input_list, expected",
+    [
+        ([4, 3, 2, 1], [1, 2, 3, 4]),
+        ([5, 8, 1, 3, 6], [1, 3, 5, 6, 8]),
+        ([1], [1]),
+        ([], []),
+        ([9, 7, 5, 3, 1], [1, 3, 5, 7, 9]),
+        ([-1, -66, 2, 64, 8], [-66, -1, 2, 8, 64]),
+    ],
+)
+def test_selection_sort(input_list, expected):
+    selection_sort(input_list)
     assert input_list == expected
